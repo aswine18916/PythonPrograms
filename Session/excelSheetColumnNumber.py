a0b9fdf0749863016b2e6e79c9690dc0e72bc65a
Aswin <80164936+aswine18916@users.noreply.github.com>
2025-05-31 11:21:05 +0530

    Update excelSheetColumnNumber.py

--- Session/excelSheetColumnNumber.py
+++ Session/excelSheetColumnNumber.py
@@ -1,18 +1,16 @@
-def let_to_num(char):
-    abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    abc=abc.lower()
-    return abc.index(char.lower()) + 1
-
-
 def titleToNumber(columnTitle):
-    if columnTitle.isalpha():
-        ans = 0
-        for i in range(len(columnTitle)):
-            ans *= 26
-            ans += let_to_num(columnTitle[i])
-        return ans
-    else:
+    if not columnTitle.isalpha():
         return None
 
+    ans = 0
+    for char in columnTitle:
+        ans = ans * 26 + (ord(char.upper()) - ord('A') + 1)
+    return ans
 
-print(titleToNumber(" "))
+# Test case
+print(titleToNumber(" "))      # Returns None
+print(titleToNumber("A"))      # 1
+print(titleToNumber("Z"))      # 26
+print(titleToNumber("AA"))     # 27
+print(titleToNumber("ZZ"))     # 702
+print(titleToNumber("AAA"))    # 703
