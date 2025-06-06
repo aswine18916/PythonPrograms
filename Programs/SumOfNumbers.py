93a1dc7207d9f4939b6b2c8c71dd8867079ebaf4
Aswin <80164936+aswine18916@users.noreply.github.com>
2025-06-06 08:20:33 +0530

    Update SumOfNumbers.py

--- Programs/SumOfNumbers.py
+++ Programs/SumOfNumbers.py
@@ -4,18 +4,19 @@ i,j,k are three numbers
 sum=i+(i-1)+(i-2)+(i-3)+...+j+(j-1)+(j-2)+(j-3)+...k
 ex: 10,8,15
 This Program finds the sum in the format 15+14+13+12+11+10+9+8+9+10"""
+
+
+
 def calculate_custom_sum(i, j, k):
     descending_part = list(range(k, j - 1, -1))
     ascending_part = list(range(j + 1, i + 1))
     total_sequence = descending_part + ascending_part
-    expression = '+'.join(str(num) for num in total_sequence)
     total_sum = sum(total_sequence)
-    print("Expression:", expression)
     print("Sum:", total_sum)
 
 
-# Example usage
+# Example
 i = 10
-j = 8
-k = 15
+j = 7
+k = 16
 calculate_custom_sum(i, j, k)
