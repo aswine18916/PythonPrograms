60c4f3f852fa4f73ee83fc00630586b4ffaaa922
Aswin <80164936+aswine18916@users.noreply.github.com>
2025-06-06 08:19:55 +0530

    Update SumOfNumbers.py

--- Programs/SumOfNumbers.py
+++ Programs/SumOfNumbers.py
@@ -4,31 +4,18 @@ i,j,k are three numbers
 sum=i+(i-1)+(i-2)+(i-3)+...+j+(j-1)+(j-2)+(j-3)+...k
 ex: 10,8,15
 This Program finds the sum in the format 15+14+13+12+11+10+9+8+9+10"""
-def sum_of_numbers(i,j,k):
+def calculate_custom_sum(i, j, k):
+    descending_part = list(range(k, j - 1, -1))
+    ascending_part = list(range(j + 1, i + 1))
+    total_sequence = descending_part + ascending_part
+    expression = '+'.join(str(num) for num in total_sequence)
+    total_sum = sum(total_sequence)
+    print("Expression:", expression)
+    print("Sum:", total_sum)
 
-    if i==j and i==k:
-        long=i+j+k
-    else:
-        list=[]
-        list.append(i)
-        list.append(j)
-        list.append(k)
-#        print(list)
 
-        l=max(list)
-#        print(l)
-        s=min(list)
-#        print(s)
-        for x  in list:
-            if x!=l and x!=s:
-                m=i
-
-#    print(l,m,s)
-        y=sum(range(s,l))
-        z=sum(range(m,l))
-        long=y+z+l
-        print(z)
-        print(y)
-    print(long)
-
-sum_of_numbers(5,8,11)
+# Example usage
+i = 10
+j = 8
+k = 15
+calculate_custom_sum(i, j, k)
